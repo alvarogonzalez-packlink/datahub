4ac5d871e28b87b09f66c4fa16230af89651e419
Pedro Silva <pedro@acryl.io>
2022-11-25 14:18:07 +0000

    Adds kafka serde classes and modifies java code to use them

--- metadata-service/factories/src/main/java/com/linkedin/gms/factory/kafka/DataHubKafkaProducerFactory.java
+++ metadata-service/factories/src/main/java/com/linkedin/gms/factory/kafka/DataHubKafkaProducerFactory.java
@@ -1,8 +1,6 @@
 package com.linkedin.gms.factory.kafka;
 
-import com.linkedin.gms.factory.kafka.schemaregistry.AwsGlueSchemaRegistryFactory;
-import com.linkedin.gms.factory.kafka.schemaregistry.KafkaSchemaRegistryFactory;
-import com.linkedin.gms.factory.kafka.schemaregistry.SchemaRegistryConfig;
+import com.datahub.kafka.avro.serializer.KafkaAvroSerializer;
 import com.linkedin.gms.factory.spring.YamlPropertySourceFactory;
 import java.util.Arrays;
 import java.util.Map;
@@ -11,39 +9,21 @@ import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
-import org.springframework.context.annotation.Lazy;
 import org.springframework.context.annotation.PropertySource;
 
 
 @Configuration
 @PropertySource(value = "classpath:/application.yml", factory = YamlPropertySourceFactory.class)
 @EnableConfigurationProperties(KafkaProperties.class)
-@Import({KafkaSchemaRegistryFactory.class, AwsGlueSchemaRegistryFactory.class})
 public class DataHubKafkaProducerFactory {
 
   @Value("${kafka.bootstrapServers}")
   private String kafkaBootstrapServers;
-
-  @Value("${kafka.schemaRegistry.type}")
-  private String schemaRegistryType;
-
-  @Autowired
-  @Lazy
-  @Qualifier("kafkaSchemaRegistry")
-  private SchemaRegistryConfig kafkaSchemaRegistryConfig;
-
-  @Autowired
-  @Lazy
-  @Qualifier("awsGlueSchemaRegistry")
-  private SchemaRegistryConfig awsGlueSchemaRegistryConfig;
 
   // CHANGE THIS
   @Bean(name = "kafkaProducer")
@@ -56,23 +36,7 @@ public class DataHubKafkaProducerFactory {
       producerProps.setBootstrapServers(Arrays.asList(kafkaBootstrapServers.split(",")));
     } // else we rely on KafkaProperties which defaults to localhost:9092
 
-    SchemaRegistryConfig schemaRegistryConfig;
-    if (schemaRegistryType.equals(KafkaSchemaRegistryFactory.TYPE)) {
-      schemaRegistryConfig = kafkaSchemaRegistryConfig;
-    } else {
-      schemaRegistryConfig = awsGlueSchemaRegistryConfig;
-    }
-
-    Map<String, Object> props = properties.buildProducerProperties();
-
-    props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, schemaRegistryConfig.getSerializer().getName());
-
-    // Override KafkaProperties with SchemaRegistryConfig only for non-empty values
-    schemaRegistryConfig.getProperties().entrySet()
-      .stream()
-      .filter(entry -> entry.getValue() != null && !entry.getValue().toString().isEmpty())
-      .forEach(entry -> props.put(entry.getKey(), entry.getValue())); 
-
-    return new KafkaProducer<>(props);
+    producerProps.setValueSerializer(KafkaAvroSerializer.class);
+    return new KafkaProducer<>(producerProps.buildProperties());
   }
 }
