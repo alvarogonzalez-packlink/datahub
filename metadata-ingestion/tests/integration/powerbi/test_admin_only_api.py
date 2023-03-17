a0520b2054a193cfd00dc3b4a2c0120f401bd946
Aseem Bansal <asmbansal2@gmail.com>
2023-03-17 23:58:05 +0530

    try to send coverage to codecov

--- metadata-ingestion/tests/integration/powerbi/test_admin_only_api.py
+++ metadata-ingestion/tests/integration/powerbi/test_admin_only_api.py
@@ -2,6 +2,7 @@ import logging
 import sys
 from typing import Any, Dict
 from unittest import mock
+import pytest
 
 from freezegun import freeze_time
 
@@ -457,6 +458,7 @@ def default_source_config():
 
 
 @freeze_time(FROZEN_TIME)
+@pytest.mark.integration
 @mock.patch("msal.ConfidentialClientApplication", side_effect=mock_msal_cca)
 def test_admin_only_apis(mock_msal, pytestconfig, tmp_path, mock_time, requests_mock):
     enable_logging()
