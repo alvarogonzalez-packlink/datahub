11a31458323610d7d9842869636c42788aa77939
Aseem Bansal <asmbansal2@gmail.com>
2023-03-31 17:28:10 +0530

    add coverage for some java folders

---
+++ .github/scripts/java_build_coverage.sh
@@ -0,0 +1,13 @@
+
+java_folders=("li-utils" "datahub-graphql-core" "datahub-upgrade")
+command="./gradlew "
+files=""
+
+for folder in "${java_folders[@]}"
+do
+    command="$command :$folder:jacocoTestReport"
+    files="${files}$(pwd)/${folder}/build/reports/jacoco/test/jacocoTestReport.xml,"
+done
+
+eval "$command build -x :metadata-ingestion:build -x :metadata-ingestion:check -x docs-website:build -x :metadata-integration:java:spark-lineage:test -x :metadata-io:test -x :metadata-ingestion-modules:airflow-plugin:build -x :datahub-frontend:build -x :datahub-web-react:build --parallel"
+echo "${files::-1}" >> $GITHUB_OUTPUT
